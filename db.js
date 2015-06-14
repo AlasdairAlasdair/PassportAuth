b0b6bc05770522a1b518dd20a5b4ad206139cb2c
Alasdair Daw <alasdair.daw@gmail.com>
2015-06-14 11:50:16 +0100

    Simplify connection string

--- db.js
+++ db.js
@@ -1,3 +1,3 @@
 module.exports = {
-  'url' : 'mongodb://' + process.env.MONGODB_USERNAME + ':' + process.env.MONGODB_PASSWORD + '@ds039281.mongolab.com:39281/scratch'
+  'url' : process.env.MONGO_LABS_CONNECTION
 }
